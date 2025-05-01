d2ffa07270aee278d41089cce559fd96b85c33ce
mazine <mazin.zougui@gmail.com>
2025-05-01 18:49:06 +0100

    Update database seeder to include categories and users

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Database\Seeders\RecipeSeeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\CategoriesSeeder;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,15 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Don't use the default factory since our User model has different columns
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
         
         // Call the recipe seeder to create sample recipes
         $this->call([
+            UserSeeder::class,
+            CategoriesSeeder::class,
             RecipeSeeder::class,
         ]);
     }
